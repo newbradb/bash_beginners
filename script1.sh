8c50113bc1870bdc45dfb5e5b1d2a8f6377df53e
newbradb <andrii@aprokofiev.xyz>
2021-04-22 17:01:39 +0300

    script taks 2.5

---
+++ script1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#bash for beginners exercise 2.5
+
+echo 'Home directory is:  '$HOME' '
+
+echo 'Terminal type:  '$TERM'' 
+
+echo 'List of services running on L3:'
+
+ls /etc/rc3.d/S*
+
+
